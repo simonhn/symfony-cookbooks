ee9645f6a0032287d3ce117c0f078d119883944c
Simon Nielsen <nielsen.simon@abc.net.au>
2014-01-28 15:18:41 +1100

    simplify php install

--- php/recipes/default.rb
+++ php/recipes/default.rb
@@ -1,31 +1,8 @@
-#
-# Author::  Joshua Timberman (<joshua@opscode.com>)
-# Author::  Seth Chisamore (<schisamo@opscode.com>)
-# Cookbook Name:: php
-# Recipe:: default
-#
-# Copyright 2009-2011, Opscode, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# remove any existing php/mysql
+execute "yum remove -y php* php-* mysql http* httpd-*"
 
-include_recipe "php::#{node['php']['install_method']}"
+# get the metadata
+execute "yum -q makecache"
 
-# update the main channels
-php_pear_channel 'pear.php.net' do
-  action :update
-end
-
-php_pear_channel 'pecl.php.net' do
-  action :update
-end
+# Install php 5.5 from aws
+execute "yum install -y php55 php55-devel php55-cli php55-snmp php55-soap php55-xml php55-xmlrpc php55-process php55-mysqlnd php55-pecl-memcache php55-opcache php55-pdo php55-imap php55-mbstring php55-intl php55-mbstring php55-mysqli"
