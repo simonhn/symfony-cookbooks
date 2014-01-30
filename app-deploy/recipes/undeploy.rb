504d1cccbfafa2a6047ac53642573a05f6fd8993
Simon Nielsen <nielsen.simon@abc.net.au>
2014-01-30 12:07:06 +1100

    pass parameters throuhg json

--- app-deploy/recipes/undeploy.rb
+++ app-deploy/recipes/undeploy.rb
@@ -1,24 +1,20 @@
-#
-# Cookbook Name:: deploy
-# Recipe:: php-undeploy
-
 include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
-
-  link "#{node[:apache][:dir]}/sites-enabled/#{application}.conf" do
-    action :delete
-    only_if do
-      ::File.exists?("#{node[:apache][:dir]}/sites-enabled/#{application}.conf")
-    end
-  end
-
-  file "#{node[:apache][:dir]}/sites-available/#{application}.conf" do
-    action :delete
-    only_if do
-      ::File.exists?("#{node[:apache][:dir]}/sites-available/#{application}.conf")
-    end
-  end
+  
+  # link "#{node[:apache][:dir]}/sites-enabled/#{application}.conf" do
+  #   action :delete
+  #   only_if do
+  #     ::File.exists?("#{node[:apache][:dir]}/sites-enabled/#{application}.conf")
+  #   end
+  # end
+  # 
+  # file "#{node[:apache][:dir]}/sites-available/#{application}.conf" do
+  #   action :delete
+  #   only_if do
+  #     ::File.exists?("#{node[:apache][:dir]}/sites-available/#{application}.conf")
+  #   end
+  # end
 
   directory "#{deploy[:deploy_to]}" do
     recursive true
