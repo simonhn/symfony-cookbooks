ddf253e760280c58e6c5782a491e9d70ef8c1f5b
Simon Nielsen <nielsen.simon@abc.net.au>
2014-04-01 19:28:32 +1100

    typos

--- app-deploy/recipes/ingest_programs.rb
+++ app-deploy/recipes/ingest_programs.rb
@@ -1,8 +1,8 @@
 include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
-  execute 'ingest_services' do
-    command 'app/console papi:ingest:service'
+  execute 'ingest_programs' do
+    command 'app/console papi:ingest:programs'
     cwd deploy[:current_path]
     user deploy[:user]
     group deploy[:group]
--- app-deploy/recipes/ingest_services.rb
+++ app-deploy/recipes/ingest_services.rb
@@ -1,8 +1,8 @@
 include_recipe 'deploy'
 
 node[:deploy].each do |application, deploy|
-  execute 'ingest_programs' do
-    command 'app/console papi:ingest:programs'
+  execute 'ingest_services' do
+    command 'app/console papi:ingest:service'
     cwd deploy[:current_path]
     user deploy[:user]
     group deploy[:group]
