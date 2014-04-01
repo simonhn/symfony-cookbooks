9a40a0f001cb3dc77656e903d23a34dda672f1bb
Simon Nielsen <nielsen.simon@abc.net.au>
2014-04-01 20:32:46 +1100

    more simplification

--- app-deploy/recipes/ingest_episodes.rb
+++ app-deploy/recipes/ingest_episodes.rb
@@ -1,10 +1,5 @@
-include_recipe 'deploy'
-
-node[:deploy].each do |application, deploy|
-  execute 'ingest_episodes' do
-    command 'app/console papi:ingest:episodes'
-    cwd deploy[:current_path]
-    user deploy[:user]
-    group deploy[:group]
-  end
+execute 'ingest_episodes' do
+  command 'app/console papi:ingest:episodes'
+  cwd '/srv/www/doctrine/current/'
+  user 'deploy'
 end
