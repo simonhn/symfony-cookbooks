8a2444afe57fac5e5eeb311f71ee7145f1c01350
Simon Nielsen <nielsen.simon@abc.net.au>
2014-04-02 14:29:25 +1100

    json parameter version

--- cronjobs/recipes/default.rb
+++ cronjobs/recipes/default.rb
@@ -5,28 +5,27 @@
 # Create custom cron jobs using configuration values in the Custom JSON
 #
 
-cron "ingest_programs_every_3_hours" do
-  minute  "20"
-  hour    "*"
-  day     "*"
-  month   "*"
-  weekday "*"
-  command "cd /srv/www/doctrine/current && app/console papi:ingest:programs 2>&1 >> /home/ec2-user/ingest_programs.log"
-  action  :create
-end
-
-
-# node[:deploy][:doctrine][:cron_jobs].each do |cron_values|
-#   cron "#{cron_values[:name]}" do
-#     minute  "#{cron_values[:minute]}"
-#     hour    "#{cron_values[:hour]}"
-#     day     "#{cron_values[:day]}"
-#     month   "#{cron_values[:month]}"
-#     weekday "#{cron_values[:weekday]}"
-#     command "#{cron_values[:command]}"
-#     user    "deploy"
-#   end
+# cron "ingest_programs_every_3_hours" do
+#   minute  "20"
+#   hour    "*"
+#   day     "*"
+#   month   "*"
+#   weekday "*"
+#   command "cd /srv/www/doctrine/current && app/console papi:ingest:programs 2>&1 >> /var/log/ingest_programs.log"
+#   action  :create
 # end
+
+
+node[:deploy][:doctrine][:cron_jobs].each do |cron_values|
+  cron "#{cron_values[:name]}" do
+    minute  "#{cron_values[:minute]}"
+    hour    "#{cron_values[:hour]}"
+    day     "#{cron_values[:day]}"
+    month   "#{cron_values[:month]}"
+    weekday "#{cron_values[:weekday]}"
+    command "#{cron_values[:command]}"
+  end
+end
 # 
 # "cron_jobs": 
 # [  
